930948ee70c97ab82fe20ea0faf345b20d8c8b40
demralp <demiryarkin@gmail.com>
2024-10-07 23:20:25 +0300

    Fixed connection issues

--- src/main/java/Prototype.java
+++ src/main/java/Prototype.java
@@ -1,31 +1,35 @@
 import java.sql.*;
 import java.util.Scanner;
 
+
 public class Prototype {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql:jdbc:mysql://localhost:3307", "root", "1234");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307", "root", "1234");
 
-            PreparedStatement userStatement = con.prepareStatement("INSERT INTO user (username, password) VALUES (?, ?, ?);");
+            PreparedStatement userStatement = con.prepareStatement("INSERT INTO user (username, password) VALUES (?, ?);");
             System.out.println("Enter Info:");
             userStatement.setString(1, s.nextLine());
             userStatement.setString(2, s.nextLine());
-            userStatement.setString(3, s.nextLine());
+
 
             userStatement.executeUpdate();
+
 
             PreparedStatement getUsers = con.prepareStatement("select * from user;");
             ResultSet getUsersResultSet = getUsers.executeQuery();
             while (getUsersResultSet.next()) {
-                int data_1 = getUsersResultSet.getInt("uid");
-                int data_2 = getUsersResultSet.getInt("username");
-                int data_3 = getUsersResultSet.getInt("password");
-                System.out.println(data_1);
-                System.out.println(data_2);
-                System.out.println(data_3);
+                int data = getUsersResultSet.getInt("uid");
+                System.out.println(data);
+                System.out.println(getUsersResultSet.getString(2));
+                System.out.println(getUsersResultSet.getString(3));
+
+
             }
+
+
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         } catch (SQLException e) {
