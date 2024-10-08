efdc9c4264ee442b8e26cc74baa58c81fc4c41bb
demralp <demiryarkin@gmail.com>
2024-10-08 12:15:16 +0300

    Added Error Checks

--- src/main/java/Prototype.java
+++ src/main/java/Prototype.java
@@ -3,16 +3,14 @@ import java.util.Scanner;
 
 
 public class Prototype {
+
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
-    public static Boolean error = false;
+
 
     public static void main(String[] args) throws ClassNotFoundException {
         Scanner s = new Scanner(System.in);
@@ -44,14 +42,10 @@ public class Prototype {
 
         } catch (SQLIntegrityConstraintViolationException e) {
             System.out.println(ANSI_RED+"Duplicate username not allowed!"+ANSI_RESET);
-            error = true;
+
 
         } catch (SQLException e) {
-            error = true;
             throw new RuntimeException(e);
-        }finally {
-            if (error = false)
-                System.out.println(ANSI_GREEN + "Data saved to Database Successfully"+ANSI_RESET);
         }
 
     }
